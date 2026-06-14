e5d8b01f575fe8913776d805187be76489aeaa36
Hammad Ansari <ansarihamad123@gmail.com>
2026-06-14 19:13:30 +0500

    dentistRoute fixed

--- server/src/routes/dentistRoute.js
+++ server/src/routes/dentistRoute.js
@@ -12,7 +12,7 @@ router.post('/register', dentistRegister);
 router.post('/login', dentistLogin);
 router.get('/verify', verifyToken('dentist'), getDentist);
 router.post('/logout', verifyToken('dentist'), dentistLogout);
-router.get('/get-dentists', verifyToken('dentist'), getAllDentists);
+router.get('/get-dentists', verifyToken('patient'), getAllDentists);
 router.get('/pending', verifyToken('admin'), isAdmin, getPendingDentists);
 router.get('/admin/all', verifyToken('admin'), isAdmin, getAllDentists);
 router.put('/:dentistId/status', verifyToken('admin'), isAdmin, updateDentistStatus);
